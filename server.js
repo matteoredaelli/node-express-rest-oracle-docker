0d802f249b5739a6e3e579492e54bbba9cb64a3c
Matteo Redaelli <matteo.redaelli@libero.it>
2017-02-09 21:32:14 +0100

    cleaning files

--- server.js
+++ server.js
@@ -11,19 +11,10 @@ app.use(bodyParser.json());
 
 
 var apis_patriot = require('./apis_patriot');
-var apis_watson_explorer = require('./apis_watson_explorer');
 
-      
 app.get('/patriot/bi_messages', apis_patriot.bi_messages);
 app.get('/patriot/dwh_status', apis_patriot.dwh_status);
 app.get('/patriot/dual', apis_patriot.dual);
-
-app.get('/watson_explorer/search/:sources/:query', apis_watson_explorer.search);
-app.get('/watson_explorer/search_mmj/:query', apis_watson_explorer.search_mmj);
-app.get('/watson_explorer/search_news/:query', apis_watson_explorer.search_news);
-app.get('/watson_explorer/search_phonebook/:query', apis_watson_explorer.search_phonebook);
-app.get('/watson_explorer/search_pnet/:query', apis_watson_explorer.search_pnet);
-app.get('/watson_explorer/search_quicklink/:query', apis_watson_explorer.search_quicklink);
 
 var server = app.listen(3000, function () {
     "use strict";
